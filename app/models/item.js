d514f15197fc1f130124496aec2ea10342503b05
Nick Trull <nick.trull@gmail.com>
2016-05-06 14:24:04 -0500

    base nav styles

--- app/models/item.js
+++ app/models/item.js
@@ -1,8 +1,11 @@
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
+import { belongsTo } from 'ember-data/relationships';
 
 export default Model.extend({
   image: attr('string'),
   title: attr('string'),
   description: attr('string'),
+
+  menu: belongsTo('menu'),
 });
