265c60d79c4afd557da49c1f9b100a29a6479034
Nick Trull <nick.trull@gmail.com>
2016-05-16 17:44:22 -0500

    set forms to toggle back to false if users leaves page without toggling

--- app/admin/menus/menu/route.js
+++ app/admin/menus/menu/route.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  setupController(controller, model) {
+    this._super(controller, model);
+
+    controller.set(`newItem`, false);
+  },
 });
