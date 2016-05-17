4c42745ed8d45809f513f84bfdcffc34317892e1
Nick Trull <nick.trull@gmail.com>
2016-05-17 18:13:09 -0500

    users menu items list is read only

--- app/users/menus/menu/route.js
+++ app/users/menus/menu/route.js
@@ -1,11 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  // cloadinary: Ember.inject.service(`profile-upload`),
-
-  setupController(controller, model) {
-    this._super(controller, model);
-
-    controller.set(`newItem`, false);
-  },
 });
