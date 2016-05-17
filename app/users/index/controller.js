b27b79924694e5d80e3fb9af3abb48dcf3bf64f9
Nick Trull <nick.trull@gmail.com>
2016-05-17 17:29:08 -0500

    users menu list is read only

--- app/users/index/controller.js
+++ app/users/index/controller.js
@@ -1,15 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  newMenu: false,
-
-  createNewMenu(attrs) {
-    const menu = this.store.createRecord(`menu`, attrs);
-
-    menu.save().then(() => {
-      this.toggleProperty(`newMenu`);
-    }).catch((err) => {
-      console.log(err);
-    });
-  },
 });
--- app/users/index/route.js
+++ app/users/index/route.js
@@ -4,10 +4,4 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll(`menu`);
   },
-
-  setupController(controller, model) {
-    this._super(controller, model);
-
-    controller.set(`newMenu`, false);
-  },
 });
