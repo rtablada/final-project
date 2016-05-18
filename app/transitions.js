34817ec9eeb9ba3603d5c8acd876e97a6dd8495d
Nick Trull <nick.trull@gmail.com>
2016-05-17 19:30:28 -0500

    liquid on hold

---
+++ app/transitions.js
@@ -0,0 +1,8 @@
+export default function(){
+  this.transition(
+    this.fromRoute('users.index'),
+    this.toRoute('users.menus.menu'),
+    this.use('toLeft'),
+    this.reverse('toRight')
+  );
+};
